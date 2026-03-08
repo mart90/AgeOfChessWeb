b45e623bdb3e2fae19a04b300ee7ac448305d928
Mart Broekkamp <martbr90@gmail.com>
2026-03-08 14:21:22 +0100

    adjust heuristics

--- policyNetwork/self_play.py
+++ policyNetwork/self_play.py
@@ -73,12 +73,14 @@ def _heuristic_score(board, move):
         piece = move[1]
         if piece == "p":
             score -= 10
+        if piece == "n":
+            score -= 5
         # if piece == "b":
         #     score += 5
         # if piece == "r":
         #     score += 10
         if piece == "q":
-            score += 15
+            score += 5
 
         # # Placing on a mine is valuable
         # dest_sq = board.squares[move[2]]
